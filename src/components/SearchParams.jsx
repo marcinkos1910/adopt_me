b1e4b56aa72fa9773758d06ab8a6f96e15a22713
Marcin Kos <marcin.kos.krk@gmail.com>
2022-04-20 22:17:35 +0200

    events on input and select

--- src/components/SearchParams.jsx
+++ src/components/SearchParams.jsx
@@ -1,12 +1,45 @@
+import { useState } from "react";
+
+const animals = ["bird", "cat", "dog", "rabbit", "reptile"]
+
+
 function searchParams(){
-    const location = "Seatle, WA";
+    const [location, setLocation] = useState("");
+    const [animal, setAnimal] = useState("")
 
     return (
         <div className="search-params">
             <form>
                 <label htmlFor="location">Location</label>
-                <input type="text" id="location" value={location} placeholder="Location"/>
+                <input 
+                    type="text" 
+                    id="location" 
+                    value={location} 
+                    placeholder="Location"
+                    onChange={(event) => {
+                        setLocation(event.target.value);
+                    }}
+                />
 
+                    <label htmlFor="animal">Animal</label>
+                    <select 
+                        id="animal" 
+                        value={animal} 
+                        onChange={(event) => {
+                            setAnimal(event.target.value);
+                        }}
+                        onBlur={(event) => {
+                            setAnimal(event.target.value);
+                        }}
+                    >   
+                        <option />
+                        {animals.map((animal) => (
+                            <option value={animal} key={animal}>
+                                {animal}
+                            </option>
+                            ))}
+                    </select>
+                    
                 <button type="submit">Submit</button>
             </form>
         </div>
