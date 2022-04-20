f4a9942453c2c8b881f0803dce774b56d9201eea
Marcin Kos <marcin.kos.krk@gmail.com>
2022-04-20 19:37:45 +0200

    add prettier

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # adopt_me
+
+
+## Dictionary
+- CDN - Content Delivery Network
+- Transpilator - (i.e. Babel, Typescript), transpilation === tranformation and compilation. Conversion between languages version.
+
+## Tools
+- Prettier - code formatter [Link](https://www.npmjs.com/package/prettier)
+- 
--- src/index.js
+++ src/index.js
@@ -1,31 +1,27 @@
 const Pet = (props) => {
-    return React.createElement("div", {}, [
-       React.createElement("h2", {}, props.name), 
-       React.createElement("h3", {}, props.animal), 
-       React.createElement("h3", {}, props.breed), 
-    ]);
-}
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {
-                name: "Luna",
-                animal: "Dog",
-                breed: "Havanese"
-            }),
-            React.createElement(Pet, {
-                name: "Pepper",
-                animal: "Bird",
-                breed: "Cockatiel"
-            }),
-            React.createElement(Pet),
-        ]
-    );
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet),
+  ]);
+};
 
 const container = document.getElementById("app");
 const root = ReactDOM.createRoot(container);
